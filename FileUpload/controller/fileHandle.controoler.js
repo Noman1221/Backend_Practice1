0f1560d6b440f339e96ba5a7de19ad776d8f193f
noman1221 <naumaank0786@gmail.com>
2026-01-15 23:31:27 +0530

    fix few changes

--- FileUpload/controller/fileHandle.controoler.js
+++ FileUpload/controller/fileHandle.controoler.js
@@ -1,18 +1,22 @@
 import File from "../model/fileHandle.model.js";
 
 export const FileController = async (req,res)=>{
-    console.log(req.file);
-    if (req.file) {
+    console.log(req);
+   
+
+    if (!req.file) {
        return res.status(404).json({message:"No File Uploaded"});
     }
     const FileAdded = new File({
        img:[
         {
-            url:url,
-            filename:filename,
+            url:req.file.path,
+            filename:req.file.filename,
         }
        ]
     })
+    console.log("is file create?", FileAdded);
+    
     await FileAdded.save();
     res.status(201).json({message:"File upload successfully"});
     
